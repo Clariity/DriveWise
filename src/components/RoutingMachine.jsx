30df5d3870e736d64867b4507164662c26c55952
rdg1g16 <rdg1g16@ecs.soton.ac.uk>
2020-04-15 01:42:51 +0100

    route line colour change

--- src/components/RoutingMachine.jsx
+++ src/components/RoutingMachine.jsx
@@ -19,7 +19,7 @@ export default function RoutingMachine({ map }) {
         lineOptions: {
           styles: [
             {
-              color: "blue",
+              color: "#2d89ef",
               opacity: 1,
               weight: 5,
             },
