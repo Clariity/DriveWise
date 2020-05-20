1ad4a3acbd24713073490e73f3d31f810f3db7f6
rdg1g16 <rdg1g16@ecs.soton.ac.uk>
2020-05-20 14:05:32 +0100

    Updates

---
+++ precache-manifest.abeaaf6077666b986fc731c729ffe415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5844e2f72a29fe0f6940b012f7f3caa6",
+    "url": "/DriveWise/index.html"
+  },
+  {
+    "revision": "2512242446f7370ccd45",
+    "url": "/DriveWise/static/css/2.6e28da81.chunk.css"
+  },
+  {
+    "revision": "111254dfc4421f51a3b2",
+    "url": "/DriveWise/static/css/main.c0965eb1.chunk.css"
+  },
+  {
+    "revision": "2512242446f7370ccd45",
+    "url": "/DriveWise/static/js/2.6afa48cd.chunk.js"
+  },
+  {
+    "revision": "f9fcca8250e2d6bfd7cc625a18ebd2aa",
+    "url": "/DriveWise/static/js/2.6afa48cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111254dfc4421f51a3b2",
+    "url": "/DriveWise/static/js/main.fc736215.chunk.js"
+  },
+  {
+    "revision": "9c6fbf4c79e1fdcb3a16",
+    "url": "/DriveWise/static/js/runtime-main.2f14cbd0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.145cf2dd2515dc3073283020e815737c.js"
+  "/DriveWise/precache-manifest.abeaaf6077666b986fc731c729ffe415.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DriveWise/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
